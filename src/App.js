9878551790a00ba323ef36b16a61f528299a52c9
unknown <priya.vegirowthu@gmail.com>
2022-05-31 14:52:25 +0530

    serach

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './Components/Login';
 import Register from './Components/Register';
 import Homepage from './Components/Homepage/Homepage';
+import Propertydetail from './Components/Forms/Propertydetail'
+import Basicinfo from './Components/Forms/Basicinfo';
+import Generalinfo from './Components/Forms/Generalinfo';
+import Locationinfo from './Components/Forms/Locationinfo';
+import Search from './Components/Homepage/Dyanamicpart/Search';
+
 
 function App() {
   return (
@@ -12,6 +18,12 @@ function App() {
         <Route path="/" element={<Login/>}/>
           <Route path='Register' element={<Register/>} />
           <Route path="Homepage" element={<Homepage/>} />
+          <Route path="Basicinfo" element={<Basicinfo/>} />
+          <Route path="Propertydetail" element={<Propertydetail/>} />
+          <Route path="Generalinfo" element={<Generalinfo/>} />
+          <Route path="Locationinfo" element={<Locationinfo/>} />
+          <Route path="Search" element={<Search/>} />
+          
           {/* 
           <Route path="contact" element={<h1></h1>} />
           <Route path="*" element={<h1></h1>} /> */}
