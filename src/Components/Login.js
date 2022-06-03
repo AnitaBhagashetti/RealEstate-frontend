89052dd4eb58b358df2f0bbb4e3ab70e15be0f7d
AnitaBhagashetti <anitabhagashetti7377@gmail.com>
2022-06-03 21:35:09 +0530

    front enc

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,22 +1,51 @@
 import React, { useState } from 'react'
 import {Link } from "react-router-dom";
-
+import axios from 'axios'
+import {useNavigate  } from "react-router-dom";
 export default function Login() {
     const [values,setValues]= useState({
         userId:"",
         password:""
     })
+    const navigate = useNavigate();
+
+
+    function handleChange(e){
+        setValues((prevstate)=>{
+            return{
+                ...prevstate,
+                [e.target.name] : e.target.value
+            }
+        })
+    }
+async function handleClick(){
+    
+    try {
+        console.log(values)
+        await axios.post('http://localhost:8000/login',{
+            userId:values.userId,
+            password:values.password
+          }).then(res=>console.log(res))
+
+        
+    } catch (error) {
+        console.log(error)
+        
+    }
+}
+
   return (
     <div className='login-main'>
         <div className='login'>
             <p className='login-logo'>Logo</p>
             <p className='login-text'>Enter your credentials to acesss your account</p>
-            <form action="" className='login-form'>
-                <input type="email" />
-                <input type="password" name="" id="" />
+            <form className='login-form' onSubmit={handleClick}>
+                <input type="email" name='userId' value={values.userId} onChange={handleChange}  />
+                <input type="password" name="password" value={values.password} onChange={handleChange}  />
+                <button className='sign-in-button' type='submit' >Sign in</button>
 
             </form>
-            <Link className='sign-in-button' to="/Homepage">Sign in</Link>
+           
         </div>
         <div className='login-register'>
             <span className='login-span-text'>Don't have account? </span>
--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import {Link } from "react-router-dom";
+import axios from 'axios'
 
 
 export default function Register() {
@@ -8,15 +9,53 @@ export default function Register() {
         password:"",
         confirmpassword:""
     })
+
+    function handleChange(e){
+     
+      setValues((prevState)=>{
+        return{
+          ...prevState,
+          [e.target.name]:e.target.value
+        }
+      })
+
+
+    }
+    async function handleSubmit(e){
+      e.preventDefault();
+      
+      try {
+       if(values && values.password===values.confirmpassword){
+         
+        await axios.post('http://localhost:8000/register',{
+          userId:values.userId,
+          password:values.password
+        }).then(res=>alert(res.data.message))
+        
+
+       }
+       else{
+         console.log("invalid user")
+       }
+      
+        
+      } catch (error) {
+        console.log(error)
+        
+      }
+    
+
+    }
   return (
     <div className='register-form'>
         <div className='register'>
         <p className='login-logo'>Logo</p>
         <p className='login-text'>create new account</p>
-        <form action="" className='form-register'>
-            <input type="email" placeholder='userId' name='userId' value={values.userId} />
-            <input type="password" name="password" value={values.password}   placeholder='password'  />
-            <input type="password" name="confirmpassword" value={values.confirmpassword}  placeholder='confirm password' />
+        <form action="" className='form-register' onSubmit={handleSubmit}>
+            <input type="email" placeholder='userId' name='userId' value={values.userId} 
+            onChange= {handleChange} />
+            <input type="password" name="password" value={values.password}   placeholder='password' onChange= {handleChange}  />
+            <input type="password" name="confirmpassword" value={values.confirmpassword}  placeholder='confirm password' onChange= {handleChange}  />
             <button className='sign-up-button' type="submit">Sign up</button>
         </form>
         </div>
