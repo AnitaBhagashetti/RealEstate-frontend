cdf89d524eea298811faa1e7aa00eb3d10d461c3
unknown <priya.vegirowthu@gmail.com>
2022-06-04 18:07:09 +0530

    formsupdated

--- src/Components/Forms/Basicinfo.js
+++ src/Components/Forms/Basicinfo.js
@@ -1,75 +1,76 @@
 import React from 'react'
 import {Link } from "react-router-dom";
+import "./Basicinfo.css";
+import Staticpart from '../Homepage/Staticpart';
 import Buttons from '../Buttons';
 import Nav from '../Homepage/Dyanamicpart/Nav';
-import Staticpart from '../Homepage/Staticpart';
 
 function Basicinfo() {
-   
   return (
-
-    <div className='basic-info'>
-        <Staticpart/>
-        <div className='basic-info-right'>
-        <Nav/>
-        <Buttons />
+<div class="main-container">
+        <Staticpart />    
         <form>
-            <div className='basic-form main'>
-        <div className='basic-info-left'>
-            <label>Property Type</label>
-            <select>
-                <option Selected>Select Property Type</option>
-                <option value="1">Land</option>
-                <option value="2">Flat</option>
-                <option value="3">Villa</option>
-            </select><br/>
-            <label>Price</label>
-            <input type="number"></input>
-            <br />
-            <label>Property Age</label>
-            <select>
-            
-                <option Selected>Select Property Age</option>
-                <option value="1">Less than 1 Year</option>
-                <option value="2">Less than 5 Year</option>
-                <option value="3">More than 5 Year</option>
-            </select><br/>
-            <label>Property Description</label>
-            <input type="text"></input>
-        </div>
-        <div className='basic-info-right'>
-            <label>Negotiable</label>
-            <select>
-                <option Selected>Select Negotiable</option>
-                <option value="1">Yes</option>
-                <option value="2">No</option>
-            </select><br/>
-            <label>Ownership</label>
-            <select>
-                <option Selected>Select Ownership</option>
-                <option value="1">Own</option>
-                <option value="2">Rented</option>
-            </select><br/>
-            <label>Property Approved</label>
-            <select>
-                <option Selected>Property Approved</option>
-                <option value="1">Yes</option>
-                <option value="2">No</option>
-            </select><br/>
-            <label>Bank Loan</label>
-            <select>
-                <option Selected>Bank Loan</option>
-                <option value="1">Yes</option>
-                <option value="2">No</option>
-            </select>
-        </div>
-        </div>
-        <Link to="/Homepage">Cancel</Link>
-        <button><Link to="/Propertydetail">Submit & Continue</Link></button>
-        </form>
-        </div>
+            <Nav />
+           <Buttons   />         
+    <div className = 'Content'>
+        <div class="sub-inline">
+            <div class="container1">
+                <label>Property Type</label><br/>
+                    <select class="selectproperty"   value={localStorage.getItem("propertyType")}onChange={(e) => localStorage.setItem("propertyType",e.target.value)}>
+                        <option Selected >Select Property Type</option>
+                        <option value="Land">Land</option>
+                        <option value="Flat">Flat</option>
+                        <option value="Villa">Villa</option>
+                    </select><br/>
+                <label>Price</label><br/>
+                    <input type="number" placeholder='Example:10000' name = "Price" value={localStorage.getItem("Price")}onChange={(e) => localStorage.setItem("Price",e.target.value)} ></input>
+                <label>Property Age</label><br/>
+                    <select class="Age" value={localStorage.getItem("PropertyAge")}onChange={(e) => localStorage.setItem("PropertyAge",e.target.value)} >
+                        <option Selected>Select Property Age</option><br/>
+                        <option value="Less than 1 Year">Less than 1 Year</option>
+                        <option value="Less than 5 Year">Less than 5 Year</option>
+                        <option value="More than 5 Year">More than 5 Year</option>
+                    </select><br/>
+                <label>Property Description</label><br/>
+                    <input type="text" name = "Propertydescription"  value={localStorage.getItem("Propertydescription")}onChange={(e) => localStorage.setItem("Propertydescription",e.target.value)}></input>
+            </div>
+            <div class="container1">
+                <label>Negotiable</label><br/>
+                    <select class="nego"    value={localStorage.getItem("Negotable")}onChange={(e) => localStorage.setItem("Negotable",e.target.value)}>
+                        <option Selected>Select Negotiable</option><br/>
+                        <option value="true">Yes</option>
+                        <option value="false">No</option>
+                    </select><br/>
+                <label>Ownership</label><br/>
+                    <select class="owner" value={localStorage.getItem("OwnerShip")}onChange={(e) => localStorage.setItem("OwnerShip",e.target.value)} >
+                        <option Selected>Select Ownership</option><br/>
+                        <option value="Own">Own</option>
+                        <option value="Rented">Rented</option>
+                    </select><br/>
+                <label>Property Approved</label><br/>
+                    <select class="property" value={localStorage.getItem("PropertyApproved")}onChange={(e) => localStorage.setItem("PropertyApproved",e.target.value)} >
+                        <option Selected>Property Approved</option><br/>
+                        <option value={true}>Yes</option>
+                        <option value={false}>No</option>
+                    </select><br/>
+                <label>Bank Loan</label><br/>
+                    <select class="bank"  value={localStorage.getItem("BankLoan")}onChange={(e) => localStorage.setItem("BankLoan",e.target.value)} >  
+                        <option Selected>Bank Loan</option><br/>
+                        <option value = {true}>Yes</option>
+                        <option value= {false}>No</option>
+                    </select>
         
+            </div>
+        </div>
+            <div class="ho">
+                <button class="sub"><Link to="/Homepage">Cancel</Link></button>
+                <button class="sub1"><Link to="/Propertydetail">Submit & Continue</Link></button>
+            </div>
     </div>
+        </form>
+        
+        
+</div>
   )
 }
 
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -7,23 +7,27 @@ export default function Login() {
         password:""
     })
   return (
-    <div className='login-main'>
-        <div className='login'>
-            <p className='login-logo'>Logo</p>
-            <p className='login-text'>Enter your credentials to acesss your account</p>
-            <form action="" className='login-form'>
+    <div className=' w-[30vw] h-[28vw]  rounded-[10px] bg-[white] relative center mt-[220px] ml-[458px]'>
+        <div className = ' bg-white '>
+            <p className='text-[#4C57B6] text-center text-4xl rounder-[3px] border-[1px] border-[#e2e2e2] border-style-[solid]mt-[3vh]'>Logo</p>
+            <p className='text-center mt-[15px]'>Enter your credentials to acesss your account</p>
+            <form action="" className='block h-[6vh] w-[24vw] ml-[3vw] '>
                 <input type="email" />
                 <input type="password" name="" id="" />
 
             </form>
-            <Link className='sign-in-button' to="/Homepage">Sign in</Link>
-        </div>
-        <div className='login-register'>
-            <span className='login-span-text'>Don't have account? </span>
-            <Link to="/Register" className='sign-up-button'>Sign up</Link>
+            <Link className='block h-[6vh] w-[24vw] ml-[3vw] rounded-[3px] mt-[9vh] pointer bg-[#4c57b6]  text-[white] text-center'  to="/Homepage">Sign in</Link>
+            <Link to="/Register" className='ml-[12vw] mt-[35px]  text-blue-600'>Sign up</Link>
         
-
         </div>
+        <div className='mt-[180px] text-center'>
+            <span className='login-span-text'>Don't have account?</span>
+            <Link to="/Register" className=''>Sign up</Link>
+            </div>
+           
+            
+
+        
        
     </div>
   )
